73951b03fa5279d18a59a5a0a2461a7b3c7d0ba5
Slobodan Bogdanovic <slobodan.bogdanovic@gmail.com>
2026-07-08 09:46:49 +0200

    fix: tooltip z index fix

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -9,16 +9,20 @@ const TooltipTrigger = TooltipPrimitive.Trigger;
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 8, ...props }, ref) => (
-  <TooltipPrimitive.Content
-    ref={ref}
-    sideOffset={sideOffset}
-    className={cn(
-      "z-50 overflow-hidden rounded-md border border-slate-700 bg-slate-950 px-3 py-2 text-sm text-slate-50 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
-      className
-    )}
-    {...props}
-  />
+>(({ className, side = "top", align = "center", sideOffset = 8, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      side={side}
+      align={align}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-[2147483647] overflow-hidden rounded-md border border-slate-700 bg-slate-950 px-3 py-2 text-sm text-slate-50 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+        className
+      )}
+      {...props}
+    />
+  </TooltipPrimitive.Portal>
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
